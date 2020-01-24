65360bf613494d64f37cc47e71b7cca368c07403
RdotSilva <RyandotSilva@gmail.com>
2020-01-24 16:20:59 -0500

    Set up state

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MaterialCommunityIcons as Icon } from "react-native-vector-icons";
 
 export default function App() {
+  const [gameState, setGameState] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+
   return (
     <View style={styles.container}>
       <View style={styles.tileContainer}>
