69b25aff9b2711f5a50b75fceddf108bee57f84f
RdotSilva <RyandotSilva@gmail.com>
2020-01-24 15:31:13 -0500

    Add styles for top row

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.tileContainer}>
-        <View style={styles.tile}></View>
-        <View style={styles.tile}></View>
-        <View style={styles.tile}></View>
+        <View style={[styles.tile, styles.topLeftTile]}></View>
+        <View style={[styles.tile, styles.topMidTile]}></View>
+        <View style={[styles.tile, styles.topRightTile]}></View>
       </View>
       <View style={styles.tileContainer}>
         <View style={styles.tile}></View>
@@ -37,5 +37,16 @@ const styles = StyleSheet.create({
   },
   tileContainer: {
     flexDirection: "row"
+  },
+  topLeftTile: {
+    borderLeftWidth: 0,
+    borderTopWidth: 0
+  },
+  topMidTile: {
+    borderTopWidth: 0
+  },
+  topRightTile: {
+    borderTopWidth: 0,
+    borderRightWidth: 0
   }
 });
