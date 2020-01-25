d7afd65ce444aa5ea4e1b04c07bcbab3f9002610
RdotSilva <RyandotSilva@gmail.com>
2020-01-25 15:16:30 -0500

    Add questions.js file

---
+++ utils/questions.js
@@ -0,0 +1,31 @@
+const questions = [
+  {
+    question: "1 + 1",
+    answers: {
+      a: "2",
+      b: "5",
+      c: "1"
+    },
+    correctAnswer: "2"
+  },
+  {
+    question: "2 + 2",
+    answers: {
+      a: "5",
+      b: "8",
+      c: "4"
+    },
+    correctAnswer: "4"
+  },
+  {
+    question: "3 + 3",
+    answers: {
+      a: "9",
+      b: "6",
+      c: "3"
+    },
+    correctAnswer: "6"
+  }
+];
+
+export default questions;
