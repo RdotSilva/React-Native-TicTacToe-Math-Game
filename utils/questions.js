8f171035765f580c2e2ad3111fd6d11a967d370c
RdotSilva <RyandotSilva@gmail.com>
2020-01-26 18:32:41 -0500

    Update math questions

--- utils/questions.js
+++ utils/questions.js
@@ -25,6 +25,60 @@ const questions = [
       c: "3"
     },
     correctAnswer: "6"
+  },
+  {
+    question: "4 + 4",
+    answers: {
+      a: "8",
+      b: "16",
+      c: "4"
+    },
+    correctAnswer: "8"
+  },
+  {
+    question: "5 + 5",
+    answers: {
+      a: "10",
+      b: "5",
+      c: "15"
+    },
+    correctAnswer: "10"
+  },
+  {
+    question: "6 + 6",
+    answers: {
+      a: "12",
+      b: "36",
+      c: "16"
+    },
+    correctAnswer: "12"
+  },
+  {
+    question: "7 + 7",
+    answers: {
+      a: "14",
+      b: "7",
+      c: "21"
+    },
+    correctAnswer: "14"
+  },
+  {
+    question: "8 + 8",
+    answers: {
+      a: "16",
+      b: "18",
+      c: "64"
+    },
+    correctAnswer: "16"
+  },
+  {
+    question: "9 + 9",
+    answers: {
+      a: "18",
+      b: "21",
+      c: "19"
+    },
+    correctAnswer: "18"
   }
 ];
 
