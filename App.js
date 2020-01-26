0996d947979ef8b3fc3fdbb6954c356d1d6094ab
RdotSilva <RyandotSilva@gmail.com>
2020-01-26 17:48:24 -0500

    Add console.log for debug on checkAnswer

--- App.js
+++ App.js
@@ -62,8 +62,11 @@ export default function App() {
 
     if (correctAnswer === answer) {
       setPlayerTurnAllowed(true);
+      console.log(`Correct! ${currentPlayer}'s turn`);
     } else {
       setPlayerTurnAllowed(false);
+      setCurrentPlayer(currentPlayer === 1 ? -1 : 1);
+      console.log(`Incorrect! ${currentPlayer}'s turn`);
     }
   };
 
